68e8069bbd0e48c31ccb89d563de5d01d18e8ae2
soungjun <jungy0309@naver.com>
2022-12-14 17:37:18 +0900

    2022/12/14 registerSeller 기능구현

--- Kmarket/src/main/webapp/member/js/memberSeller.js
+++ Kmarket/src/main/webapp/member/js/memberSeller.js
@@ -4,181 +4,226 @@
    내용 : 사용자 회원가입 유효성 검사 
  */
 // 데이터 검증에 사용하는 정규표현식
-let regUid   = /^[a-z]+[a-z0-9]{5,19}$/g;
-let regPass  = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{5,16}$/;
-let regName  = /^[가-힣]+$/ 
-let regNick  = /^[a-zA-Zㄱ-힣0-9][a-zA-Zㄱ-힣0-9]*$/;
-let regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-let regHp    = /^01(?:0|1|[6-9])-(?:\d{4})-\d{4}$/;
+const reUid = /^[a-z]+[a-z0-9]{5,19}$/g;
+const rePass = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+]).{8,255}$/;
+const reCompany = /^[\w\Wㄱ-ㅎㅏ-ㅣ가-힣]{2,20}$/;
+const reCeo = /^[ㄱ-힣]+$/;
+const reTel = /^\d{2,3}-\d{3,4}-\d{4}$/;
+const reFax =  /^\d{2,3}-\d{3,4}-\d{4}$/;
+const reEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+const reName = /^[ㄱ-힣]+$/;
+const reHp = /^01(?:0|1|[6-9])-(?:\d{4})-\d{4}$/;
+
 
 // 폼 데이터 검증 결과 상태변수
-// 폼 데이터 검증 결과 상태변수
-let isUidOk   = false;
-let isPassOk  = false;
-let isNameOk  = false;
-let isNickOk  = false;
+let isUidOk 	= false;
+let isPassOk 	= false;
+let isCompanyOk = false;
+let isCeoOk 	= false;
+let isTelOk = false;
+let isFaxOk = false;
 let isEmailOk = false;
-let isHpOk    = false;
-let isEmailAuthOk = false;
-let isEmailAuthCodeOk = false;
-let receivedCode = 0;	
+
+function handleOnInput(el, maxlength) {
+	if(el.value.length > maxlength) {
+		el.value = el.value.substr(0, maxlength);
+	}
+}
 
 $(function(){
 	
-	// 아이디 유효성 검증 & 중복체크
-	$('input[name=uid]').keydown(function(){
+	// 아이디 검사하기
+	$('input[name=km_uid]').change(function(){
 		isUidOk = false;
+		$('.msgId').text('');
 	});
 	
-	$('#btnUidCheck').click(function(){
+	$('input[name=km_uid]').change(function(){
 		
-		let uid = $('input[name=uid]').val();
-		//alert(uid);
-		
-		if(isUidOk){
+		if(isUidOk) {
 			return;
 		}
 		
-		if(!uid.match(regUid)){
+		let uid = $('input[name=km_uid]').val();
+		
+		if (!uid.match(reUid)) {
 			isUidOk = false;
-			$('.resultUid').css('color', 'red').text('아이디가 유효하지 않습니다.');
+			$('.msgId').css('color','red').text('유효한 아이디가 아닙니다.');
 			return;
 		}
 		
-		let jsonData = {"uid":uid};
+		let jsonData = {
+				"km_uid" : uid
+		};
 		
-		$('.resultUid').css('color', 'black').text('...');
-		
-		setTimeout(()=>{
+		$('.msgId').css('color','black').text('...');
 			
-			$.ajax({
-				url: '/Kmarket/member/checkUid.do',
-				method: 'get',
-				data: jsonData,
-				dataType: 'json',
-				success:function(data){
-					//console.log(data);
-					if(data.result == 0){
+		$.ajax({
+			url: '/Kmarket/member/checkUid.do',
+			method:'get',
+			data: jsonData,
+			dataType:'json',
+			success:(data)=>{
+					if(data.result == 0) {
 						isUidOk = true;
-						$('.resultUid').css('color', 'green').text('사용 가능한 아이디 입니다.');
-					}else{
+						$('.msgId').css('color','green').text('사용 가능한 아이디 입니다.');
+					} else {
 						isUidOk = false;
-						$('.resultUid').css('color', 'red').text('이미 사용중인 아이디 입니다.');
+						$('.msgId').css('color','red').text('이미 사용중인 아이디 입니다.');
 					}
-				}
-			});
-			
-		}, 500);
-	});		
-	
-	// 비밀번호 일치여부 확인
-	$('input[name=pass2]').focusout(function(){			
-		let pass1 = $('input[name=pass1]').val();
-		let pass2 = $(this).val();
-		
-		if(pass1 == pass2){
-							
-			if(pass2.match(regPass)){
-				isPassOk = true;
-				$('.resultPass').css('color', 'green').text('비밀번호가 일치합니다.');	
-			}else{
-				isPassOk = false;
-				$('.resultPass').css('color', 'red').text('영문, 숫자, 특수문자 조합 최소 5자 이상 이어야 합니다.');
-			}				
-			
-		}else{
-			isPassOk = false;
-			$('.resultPass').css('color', 'red').text('비밀번호가 일치하지 않습니다.');
-		}			
+			}
+		});
 	});
 	
-	// 이름 유효성 검증
-	$('input[name=name]').focusout(function(){
+	// 비밀번호 검사하기
+	$('input[name=km_pass2]').focusout(function(){
+		let pass1 = $('input[name=km_pass]').val();
+		let pass2 = $('input[name=km_pass2]').val();
 		
+		if(pass2.match(rePass)) {
+			if(pass1 == pass2) {
+				isPassOk = true;
+				$('.msgPass').css('color', 'green').text('사용하실 수 있는 비밀번호 입니다.');
+			} else {
+				isPassOk =false;
+				$('.msgPass').css('color', 'red').text('비밀번호가 일치하지 않습니다.');
+			}
+		} else {
+			isPassOk = false;
+			$('.msgPass').css('color', 'red').text('비밀번호는 숫자,영문,특수문자 포함 8자리 이상 이어야 합니다.');
+		}
+	});
+	
+	// 회사명 검사하기
+	$('input[name=kms_company]').focusout(function(){
+		let company = $(this).val();
+		
+		if(company.match(reCompany)) {
+			isCompanyOk = true;
+			$('.msgCompany').text('사용 가능한 회사명입니다.');
+		} else {
+			isCompanyOk = false;
+			$('.msgCompany').css('color','red').text('유효한 회사명이 아닙니다.');
+		}
+	});
+	
+	// 대표자 검사하기
+	$('input[name=kms_ceo]').focusout(function(){
+		const ceo = $(this).val();
+		
+		if (ceo.match(reCeo)) {
+			isCeoOk = true;
+			$('.msgTel').text('');
+		} else {
+			isCeoOk = false;
+			$('.msgCeo').css('color','red').text('유효한 대표자명이 아닙니다.');
+		}
+	});
+	
+	// 전화번호 검사하기
+	$('input[name=kms_tel]').focusout(function(){
+		const tel = $(this).val();
+		
+		if (tel.match(reTel)) {
+			isTelOk = true;
+			$('.msgTel').text('');
+		} else {
+			isTelOk = false;
+			$('.msgTel').css('color','red').text('유효한 전화번호가 아닙니다.');
+		}
+	});
+	
+	// 팩스번호 검사하기
+	$('input[name=kms_fax]').focusout(function(){
+		const fax = $(this).val();
+		
+		if (fax.match(reFax)) {
+			isFaxOk = true;
+			$('.msgFax').text('');
+		} else {
+			isFaxOk = false;
+			$('.msgFax').css('color','red').text('유효한 팩스번호가 아닙니다.');
+		}
+	});
+	
+	// 이메일 검사하기
+	$('input[name=kms_email]').focusout(function(){
+		const email = $(this).val();
+		
+		if (email.match(reEmail)) {
+			isEmailOk = true;
+			$('.msgEmail').text('');
+		} else {
+			isEmailOk = false;
+			$('.msgEmail').css('color','red').text('유효한 이메일이 아닙니다.');
+		}
+	});
+	
+	// 이름 검사하기
+	$('input[name=km_name]').focusout(function(){
 		let name = $(this).val();
 		
-		if(!name.match(regName)){
-			isNameOk = false;
-			$('.resultName').css('color', 'red').text('이름은 한글 2자 이상 이어야 합니다.');
-		}else{
+		if(name.match(reName)) {
 			isNameOk = true;
-			$('.resultName').text('');
+			$('.msgName').text('사용 가능한 이름입니다.');
+		} else {
+			isNameOk = false;
+			$('.msgName').css('color','red').text('유효한 이름이 아닙니다.');
 		}
 	});
 	
 	
-	
-	// 이메일 유효성 검사
-	$('input[name=email]').focusout(function(){
-		let email = $(this).val();
+	// 휴대폰 검사하기
+	$('input[name=km_hp]').focusout(function(){
+		const hp = $(this).val();
 		
-		if(!email.match(regEmail)){
-			isEmailOk = false;
-			$('.resultEmail').css('color', 'red').text('이메일이 유효하지 않습니다.');
-		}else{
-			isEmailOk = true;
-			$('.resultEmail').text('');
-		}			
-	});
-	
-	
-	// 휴대폰 유효성 검사
-	$('input[name=hp]').focusout(function(){
-		let hp = $(this).val();
-		
-		if(!hp.match(regHp)){
-			isHpOk = false;
-			$('.resultHp').css('color', 'red').text('휴대폰이 유효하지 않습니다.');
-		}else{
+		if (hp.match(reHp)) {
 			isHpOk = true;
-			$('.resultHp').text('');
+			$('.msgHp').text('');
+		} else {
+			isHpOk = false;
+			$('.msgHp').css('color','red').text('유효한 번호가 아닙니다.');
 		}
 	});
 	
-	// 폼 전송이 시작될 때 실행되는 폼 이벤트(폼 전송 버튼을 클릭했을 때) 
+	// 최종 폼 전송할 때
 	$('.register > form').submit(function(){
-					
-		////////////////////////////////////
-		// 폼 데이터 유효성 검증(Validation)
-		////////////////////////////////////
-		// 아이디 검증
-		if(!isUidOk){
-			alert('아이디를 확인 하십시요.');
+		// ID 검증
+		if(!isUidOk) {
+			alert('아이디가 유효하지 않습니다.');
 			return false;
 		}
 		// 비밀번호 검증
-		if(!isPassOk){
-			alert('비밀번호를 확인 하십시요.');
+		if(!isPassOk) {
+			alert('비밀번호가 유효하지 않습니다.');
 			return false;
 		}
-		// 이름 검증
-		if(!isNameOk){
-			alert('이름을 확인 하십시요.');
+		// 회사명 검증
+		if(!isCompanyOk) {
+			alert('회사명이 유효하지 않습니다.');
 			return false;
 		}
-		// 별명 검증
-		if(!isNickOk){
-			alert('별명을 확인 하십시요.');
+		// 대표자 검증
+		if(!isCeoOk) {
+			alert('대표자명이 유효하지 않습니다.');
 			return false;
 		}
-		// 이메일 검증
-		if(!isEmailOk){
-			alert('이메일을 확인 하십시요.');
+		// 전화번호 검증
+		if(!isTelOk) {
+			alert('전화번호가 유효하지 않습니다.');
 			return false;
 		}
-		// 이메일 인증코드 검증
-		if(!isEmailAuthCodeOk){
-			alert('이메일을 인증을 수행 하십시요.');
+		// 팩스번호 검증
+		if(!isFaxOk) {
+			alert('전화번호가 유효하지 않습니다.');
 			return false;
 		}
-		// 휴대폰 검증
-		if(!isHpOk){
-			alert('휴대폰을 확인 하십시요.');
+		// EMAIL 검증
+		if(!isEmailOk) {
+			alert('이메일이 유효하지 않습니다.');
 			return false;
 		}
 		
-		// 최종 전송
 		return true;
 	});
 });
- 
