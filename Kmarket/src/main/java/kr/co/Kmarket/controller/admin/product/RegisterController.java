1503b74856e1b59c73a2c653780499f9e588e043
hochmoon <s01099760587@gmail.com>
2022-12-14 12:05:46 +0900

    2022/12/14 심규영 product/view.do 미완성

--- Kmarket/src/main/java/kr/co/Kmarket/controller/admin/product/RegisterController.java
+++ Kmarket/src/main/java/kr/co/Kmarket/controller/admin/product/RegisterController.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.oreilly.servlet.MultipartRequest;
-
 import kr.co.Kmarket.service.admin.AdminService;
 import kr.co.Kmarket.vo.ProductVO;
 
