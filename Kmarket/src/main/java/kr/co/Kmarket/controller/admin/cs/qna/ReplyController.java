69da00ff4d4bd22cd160a6fd643ecb838acedec6
hochmun <s01099760587@gmail.com>
2022-12-19 16:13:11 +0900

    2022/12/19 심규영 admin/ _header _footer 정리

--- Kmarket/src/main/java/kr/co/Kmarket/controller/admin/cs/qna/ReplyController.java
+++ Kmarket/src/main/java/kr/co/Kmarket/controller/admin/cs/qna/ReplyController.java
@@ -8,13 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/admin/cs/qna/reqly.do")
+@WebServlet("/admin/cs/qna/reply.do")
 public class ReplyController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/admin/cs/qna/reqly.jsp").forward(req, resp);
+		req.getRequestDispatcher("/admin/cs/qna/reply.jsp").forward(req, resp);
 	}
 }
--- Kmarket/src/main/webapp/admin/cs/faq/list.jsp
+++ Kmarket/src/main/webapp/admin/cs/faq/list.jsp
@@ -1,5 +1,5 @@
 <%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8"%>
-<jsp:include page="./_header.jsp"/>
+<jsp:include page="../../_header.jsp"/>
     <section id="admin-product-list">
                 <nav>
                     <h3>자주묻는질문 작성</h3>
@@ -60,4 +60,4 @@
             </section>
     </div>
 </section>
-<jsp:include page="./_footer.jsp"/>
+<jsp:include page="../../_footer.jsp"/>
