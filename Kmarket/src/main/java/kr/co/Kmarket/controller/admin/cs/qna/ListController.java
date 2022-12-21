b743eb4103c34e27a98fffedd5e26983a2473469
realKimJaeJUN <nagandr1113@gmail.com>
2022-12-21 19:33:40 +0900

    22/12/21 admin/cs 작업중

--- Kmarket/src/main/java/kr/co/Kmarket/controller/admin/cs/qna/ListController.java
+++ Kmarket/src/main/java/kr/co/Kmarket/controller/admin/cs/qna/ListController.java
@@ -1,6 +1,7 @@
 package kr.co.Kmarket.controller.admin.cs.qna;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,13 +9,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.co.Kmarket.service.admin.AdminServiceQna;
+import kr.co.Kmarket.vo.cs.CsCate1VO;
+import kr.co.Kmarket.vo.cs.CsQnaVO;
+
 @WebServlet("/admin/cs/qna/list.do")
 public class ListController extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+	private AdminServiceQna service = AdminServiceQna.INSTANCE;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String cate1 = req.getParameter("cate1");
+		// 페이징
+		String pg = req.getParameter("pg");
+		if(pg == null || pg.trim().equals("")){
+			pg = "1";
+			}
+		
+		CsCate1VO vos = service.selectCsCate(cate1);
+		int total = service.selectCountTotal(cate1);
+		int limitStart = service.boardPaging(req, cate1);
+		
+		List<CsQnaVO> QnaArts = null;
+		QnaArts = service.selectQnaArticles(cate1, limitStart);
+		
+		req.setAttribute("cate1", cate1);
+		req.setAttribute("QnaArts", QnaArts);
+		req.setAttribute("QnaArts", service.selectQnaArticles(cate1, limitStart));
+		req.setAttribute("pg", pg);
+		req.setAttribute("vos", vos);
+		req.setAttribute("total", total);
+		req.setAttribute("limitStart", limitStart);
+		
 		req.getRequestDispatcher("/admin/cs/qna/list.jsp").forward(req, resp);
 	}
 }
---
+++ Kmarket/src/main/java/kr/co/Kmarket/service/admin/AdminServiceQna.java
@@ -0,0 +1,101 @@
+package kr.co.Kmarket.service.admin;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import kr.co.Kmarket.dao.cs.CsDAO;
+import kr.co.Kmarket.vo.cs.CsCate1VO;
+import kr.co.Kmarket.vo.cs.CsQnaVO;
+
+public enum AdminServiceQna {
+
+	INSTANCE;
+	private CsDAO dao;
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	private AdminServiceQna() {
+		dao = new CsDAO();
+	}
+	
+	// create
+	
+	// read
+	/**
+	 * 2022/12/19 qna 작성글 불러오기
+	 * @author 김재준
+	 * @param qnaNo
+	 * @return
+	 */
+	public CsQnaVO selectQnaArticle(String qnaNo) {
+		return dao.selectQnaArticle(qnaNo);
+	}
+	
+	/**
+	 * 2022/12/21 카테고리 이름 불러오기
+	 * @author 김재준
+	 * @param cate1name
+	 * @return
+	 */
+	public CsCate1VO selectCsCate(String cate1) {
+		return dao.selectCsCate(cate1);
+	}
+	
+	// service
+	
+	/** 페이징 */ 
+	
+	/**
+	 * 2022/12/19 qna 작성글 목록 불러오기
+	 * @author 김재준
+	 * @param cate1
+	 * @param limitStart
+	 * @return
+	 */
+	public List<CsQnaVO> selectQnaArticles(String cate1, int limitStart){
+		return dao.selectQnaArticles(cate1, limitStart);
+	}
+	
+	public int selectCountTotal(String cate1) {
+		return dao.selectCountTotal(cate1);
+	}
+	
+	public int boardPaging(HttpServletRequest req, String cate1) {
+		String pg = req.getParameter("pg");
+		
+		int currentPage = 1; // 현재 페이지
+		int total = selectCountTotal(cate1); // 총 게시물 갯수
+		int lastPageNum = 0; // 마지막 페이지 번호
+		
+		// 페이지 마지막 번호 계산
+		if(total % 10 != 0) lastPageNum = (total / 10) + 1;
+		else lastPageNum = (total / 10);
+		
+		// 전체 페이지 게시물 limit 시작값 계산
+		if(pg != null) currentPage = Integer.parseInt(pg);
+		int limitStart = (currentPage - 1) * 10;
+		
+		// 페이지 그룹 계산
+		int pageGroupCurrent = (int)Math.ceil(currentPage / 5.0);
+		int pageGroupStart = (pageGroupCurrent - 1) * 5 + 1;
+		int pageGroupEnd = pageGroupCurrent * 5;
+		
+		if (pageGroupEnd > lastPageNum) pageGroupEnd = lastPageNum;
+		
+		// 페이지 시작 번호 계산
+		int pageStartNum = total - limitStart;
+		
+		req.setAttribute("lastPageNum", lastPageNum);
+		req.setAttribute("currentPage", currentPage);
+		req.setAttribute("pageGroupCurrent", pageGroupCurrent);
+		req.setAttribute("pageGroupStart", pageGroupStart);
+		req.setAttribute("pageGroupEnd", pageGroupEnd);
+		req.setAttribute("pageStartNum", pageStartNum);
+		
+		return limitStart;
+	}
+	
+}
